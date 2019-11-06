29bb216b3cec4c5c16426b4d4de7820d96a0a1cc
Giovanny <giovankyandres@gmail.com>
2019-11-05 23:38:13 -0300

    secure commit

--- frontend/src/components/Front.vue
+++ frontend/src/components/Front.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Front</h1>
+    <img src="../assets/front.png">
     <router-link to="/signin" tag="button">Iniciar Sesion</router-link> |
     <router-link to="/signup" tag="button">Registrarse</router-link>
     <router-view/>
@@ -15,28 +16,27 @@ export default Vue.extend({
 })
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-button{
-  border: 3px solid #2c3e50;
-  border-radius: 6px;
-  background: none;
-  color: #2c3e50;
-  font-weight: bolder;
-  padding: .3rem;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  button{
+    border: 3px solid #2c3e50;
+    border-radius: 6px;
+    background: none;
+    color: #2c3e50;
+    font-weight: bolder;
+    padding: .3rem;
+  }
 </style>
